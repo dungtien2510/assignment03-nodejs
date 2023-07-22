b44c30931472c77efc8be0aa87787c44e8cdd05a
tiendung <dungtien2510@gmail.com>
2023-07-22 20:03:18 +0700

    add controller postLogin (auth)

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -5,8 +5,65 @@ const { validationResult } = require("express-validation");
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  // const errors = validationResult(req);:
+  // Hàm này sử dụng express-validator để kiểm tra và xác thực dữ liệu người dùng.
+  // validationResult là một hàm trong express-validator giúp kiểm tra xem liệu có lỗi nào trong các trường đã nhập hay không.
   const errors = validationResult(req);
+
+  //if (!errors.isEmpty()) { ... }:
+  // Đoạn mã này kiểm tra xem có lỗi nào trong dữ liệu người dùng hay không.
+  // Nếu có lỗi (mảng errors không trống),
+  // chương trình sẽ xử lý lỗi và trả về một đối tượng JSON báo lỗi về phía người dùng.
   if (!errors.isEmpty()) {
+    //console.log(errors.array());
+    //: Hàm này in ra mảng các lỗi dưới dạng mảng JSON trong bản ghi console của máy chủ.
+    // Điều này giúp bạn dễ dàng xem và debug lỗi nếu có.
     console.log(errors.array());
+
+    //return res.status(422).json({ ... }):
+    // Trả về một phản hồi JSON cho người dùng với mã trạng thái 422 (Unprocessable Entity) để báo lỗi.
+    // Phản hồi JSON này bao gồm thông báo lỗi đầu tiên từ mảng errors, dữ liệu đã nhập (oldInput),
+    // và tất cả các lỗi trong mảng errors (validationErrors).
+    return res.status(422).json({
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+      },
+      validationErrors: errors.array(),
+    });
   }
+
+  //User.findOne({email: email}): Đoạn mã này tìm kiếm trong cơ sở dữ liệu để lấy thông tin người dùng có email trùng khớp.
+  User.findOne({ email: email })
+    .then((user) => {
+      //Hàm này sử dụng bcrypt để so sánh mật khẩu đã nhập của người dùng với mật khẩu đã lưu trong cơ sở dữ liệu.
+      // Nếu hai mật khẩu khớp nhau, doMatch sẽ là true, và ngược lại.
+      bcrypt.compare(password, user.password).then((doMatch) => {
+        if (doMatch) {
+          // // Nếu mật khẩu khớp, đánh dấu người dùng đã đăng nhập và lưu thông tin người dùng vào session
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save((err) => {
+            console.log(err);
+
+            // // Lưu session và trả về một phản hồi JSON thành công với mã trạng thái 401
+            res.status(401).json({ successMessage: "success" });
+          });
+        }
+
+        //req.flash("error", "Invalid email or password") được sử dụng để đặt thông báo lỗi (error message) vào flash session trong ứng dụng sử dụng framework Express. Flash session là một dạng session đặc biệt trong Express, cho phép lưu trữ dữ liệu tạm thời để hiển thị cho người dùng trong một yêu cầu HTTP duy nhất, thường được sử dụng để hiển thị thông báo sau khi xảy ra một hành động nhất định (ví dụ: đăng nhập thất bại).
+        //Khi bạn gọi req.flash("error", "Invalid email or password"), bạn đang đặt thông báo lỗi vào flash session với khóa là "error" và giá trị là "Invalid email or password". Sau đó, thông báo lỗi này sẽ tồn tại trong flash session và sẽ được sử dụng để hiển thị cho người dùng sau khi trình duyệt chuyển đổi sang trang khác (thông thường là sau khi gửi một yêu cầu và nhận được phản hồi).
+        req.flash("error", "Invalid email or password");
+        res.status(403).json({ successMessage: "Invalid password" });
+      });
+    })
+
+    //Nếu có lỗi trong quá trình xử lý, chương trình sẽ tạo một đối tượng Error với mã trạng thái 500 và gửi nó cho middleware next để xử lý lỗi.
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
